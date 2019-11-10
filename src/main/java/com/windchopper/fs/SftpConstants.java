90cba89a5a06bc02449aecd49c733d2847ffce06
wind <wind@comtv.ru>
2019-11-10 03:35:33 +0300

    #

--- src/main/java/com/windchopper/fs/SftpConstants.java
+++ src/main/java/com/windchopper/fs/SftpConstants.java
@@ -1,7 +1,5 @@
 package com.windchopper.fs;
 
-import java.time.Duration;
-
 public interface SftpConstants {
 
     String HOST = "Sftp.host";
@@ -9,11 +7,9 @@ public interface SftpConstants {
     String USERNAME = "Sftp.username";
     String PASSWORD = "Sftp.password";
     String BUFFER_SIZE = "Sftp.bufferSize";
-    String BUFFER_LIFETIME = "Sftp.bufferLifetime";
 
     int DEFAULT_PORT = 22;
     int DEFAULT_BUFFER_SIZE = 1000;
-    Duration DEFAULT_BUFFER_LIFETIME = Duration.ofMinutes(10);
 
     String SCHEME = "sftp";
     String SEPARATOR = "/";
